2165c32c320647518845057e4ffad81eab66eeae
Francisco Costa <francisco@fcosta.pt>
2019-02-26 22:27:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,15 @@ def batch_badge_creator(array)
   end
   badge_messages
 end
+
+def conference_badges(speakers)
+  rooms = [1,2,3,4,5,6,7]
+  room_assignments = []
+  index = 0
+  while index < rooms.length - 1
+    
+
+
+  "Hello, #{i}! You'll be assigned to room "
+  
+end
