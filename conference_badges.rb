db94e67f8fd32eeda8e0b23792418fc4da61d3f7
Francisco Costa <francisco@fcosta.pt>
2019-02-26 22:46:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(speakers)
-  speakers.each_with_index do |speakers, index|
+  speakers.each_with_index.map do |speakers, index|
   "Hello, #{speakers}! You'll be assigned to room #{index+1}!"
 end
 end
